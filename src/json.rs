f30edbe812f87d94548a7052abde742414875818
FelipeMCassiano <felipemcassiano@gmail.com>
2024-06-17 12:39:29 -0300

    feat: json_bool, json_string and curlybraces

--- src/json.rs
+++ src/json.rs
@@ -1,4 +1,8 @@
-use nom::Parser;
+use core::str;
+
+use nom::{branch::alt, character::complete::digit1, IResult};
+
+use crate::parser::curly_braces;
 
 pub enum Json {
     Null,
@@ -9,4 +13,6 @@ pub enum Json {
     Object(std::collections::HashMap<String, Json>),
 }
 
-pub fn json<T>(input: &str) -> impl Parser<Json, T, T> {}
+pub fn json(input: &str) -> IResult<&str, &str> {
+    alt((curly_braces, digit1))(input)
+}
--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,36 @@
-use nom::{bytes::complete::is_not, character::complete::char, sequence::delimited, IResult};
+use std::char;
 
+use nom::{
+    branch::alt,
+    bytes::complete::{is_not, tag},
+    character::complete::{char, satisfy},
+    combinator::map,
+    multi::many0,
+    sequence::delimited,
+    IResult,
+};
+// todo: change the return type to str String
 pub fn curly_braces(input: &str) -> IResult<&str, &str> {
     delimited(char('{'), is_not("}"), char('}'))(input)
 }
 
 #[test]
-fn parens_test() {
+fn curly_braces_test() {
     assert_eq!(curly_braces("{felipe : oi}"), Ok(("", "felipe : oi")))
+}
+
+pub fn json_string(input: &str) -> IResult<&str, String> {
+    delimited(
+        char('"'),
+        map(many0(satisfy(|c| c != '"')), |chars: Vec<char>| {
+            chars.into_iter().collect::<String>()
+        }),
+        char('"'),
+    )(input)
+}
+
+pub fn json_bool(input: &str) -> IResult<&str, bool> {
+    alt((map(tag("false"), |_| false), map(tag("true"), |_| false)))(input)
 }
 
 // parse to tranform string to i32
